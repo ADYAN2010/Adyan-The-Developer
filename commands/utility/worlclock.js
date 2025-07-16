c50563a4077d87203d049ee745c3c7b46be9b592
Adyan <106177764+ADYAN2010@users.noreply.github.com>
2025-07-16 14:26:41 +0600

    AdyanTheDeveloper

--- commands/utility/worlclock.js
+++ commands/utility/worlclock.js
@@ -156,8 +156,8 @@ module.exports = {
 /_______  /\___|_  /|___|  \___/\____|__  / 
         \/       \/                     \/  
                     
-DISCORD :  https://discord.com/invite/xQF9f9yUEM                   
-YouTube : https://www.youtube.com/@GlaceYT                         
+DISCORD :  https://discord.gg/uBP5shzG2k          
+YouTube :  https://www.youtube.com/@adyan2010dev                          
 
 Command Verified : ✓  
 Website        : ssrr.tech  
